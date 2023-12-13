49e4889bb1291723ce0f3485e3f7a11a546f7501
KreiZi0 <steven.mirontsuk1@gmail.com>
2023-12-13 15:24:37 +0200

    Created React app + Firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsvrS9PlwVxaBmX46qRXmlFtdrmDQYplU",
+  authDomain: "airbnbcopy-d9e4a.firebaseapp.com",
+  projectId: "airbnbcopy-d9e4a",
+  storageBucket: "airbnbcopy-d9e4a.appspot.com",
+  messagingSenderId: "377975085437",
+  appId: "1:377975085437:web:e7b842e09af3dfe5189414"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
