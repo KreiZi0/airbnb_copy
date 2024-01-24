d15c359e13b186d3be822b22852ec50d151c692c
KreiZi0 <steven.mirontsuk1@gmail.com>
2024-01-24 19:59:24 +0200

    "add-hotel" route access

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-//App.js
-import React from 'react';
+// App.js
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AddHotel from './components/AddHotel';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+function PrivateRoute({ element }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return user ? element : null;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add-hotel" element={<AddHotel />} />
+        <Route
+          path="/add-hotel"
+          element={<PrivateRoute element={<AddHotel />} />}
+        />
       </Routes>
     </BrowserRouter>
   );
